ed52ef4b5e58a2b21f15f1a595200384dc2bba23
Pallavi Kandikanti <pallavikandikantica@gmail.com>
2025-07-29 21:31:10 -0400

    Added interfaces, services, migrations, controllers, database data

---
+++ Opinion-on-Quotes/Models/Comment.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Opinion_on_Quotes.Models;
+
+
+namespace Opinion_on_Quotes.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        public string? CommentText { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        //A comment belongs to one topic
+
+        public required virtual Quote Quote { get; set; }
+        public int quote_id { get; set; }
+
+        // reference to Identity user
+        public string? UserId { get; set; }  // Identity uses string for user IDs
+    }
+
+
+    public class CommentDto
+    {
+        public int CommentId { get; set; }
+        public string CommentText { get; set; } = "No comment provided.";
+        public string CreatedAt { get; set; } = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+
+        // Temporarily hardcode
+        public string UserName { get; set; } = "Anonymous";
+    }
+
+    public class CreateCommentDto
+    {
+        // Defaults if user doesn't provide comment
+        public string CommentText { get; set; } = "No comment provided.";
+
+
+        public int quote_id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
---
+++ Opinion-on-Quotes/Models/Quote.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Opinion_on_Quotes.Models
+{
+    public class Quote
+    {
+        [Key]
+        public int quote_id { get; set; }
+
+        //Each quote belongs to one drama
+        [ForeignKey("drama_id")]
+        public required virtual Drama Drama { get; set; }
+
+        public int drama_id { get; set; }
+
+        public string? content { get; set; }
+        public string? actor { get; set; }
+        public int episode { get; set; }
+
+        //A quote can have many moods
+        [System.Text.Json.Serialization.JsonIgnore]
+        public ICollection<QuoteMood>? QuoteMoods { get; set; }
+    }
+    public class QuoteDto
+    {
+        public int quote_id { get; set; }
+        public string? content { get; set; }
+        public string? actor { get; set; }
+        public int episode { get; set; }
+        public string? title { get; set; }
+
+        public int drama_id { get; set; }
+    }
+}
